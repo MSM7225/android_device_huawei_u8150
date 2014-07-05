0f5b3a174150dd3cd5789b5685fc01fa4c72a6d0
Alberto96 <albertoguanti@gmail.com>
2014-07-05 17:39:28 +0200

    u8160: mm-core: add OMX support

--- mm-core/Android.mk
+++ mm-core/Android.mk
@@ -1,6 +1,6 @@
 OMX_CORE_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-ifneq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifeq ($(BOARD_USES_QCOM_OMXCORE),true)
 include $(OMX_CORE_PATH)/omxcore/Android.mk
 endif
